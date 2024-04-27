5f0ee24b2c00b697542d03732f6d2f4cca6dd305
starskill-1220 <contact@star-skill.jp>
2024-04-27 14:35:23 +0900

    Implement basic login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      reset_session
+      log_in user
+      flash[:success] = "ログイン完了"
+      redirect_to user
+    else
+      flash.now[:danger] = "メールアドレス、またはパスワードが違います"
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to root_url, status: :see_other
+  end
+end
